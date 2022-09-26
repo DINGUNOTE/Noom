d6b07f757e26c5864b2550f9754c96c730dcc90e
web.ljg <web.ljg@gmail.com>
2022-09-26 21:03:52 +0900

    feat: back-end <-> front-end 메세지 보내기

---
+++ src/public/js/app.js
@@ -0,0 +1,15 @@
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener('open', () => console.log('Connected to Server 😀'));
+
+socket.addEventListener('message', (message) => {
+  console.log('New Message: ', message.data, 'from the server');
+});
+
+socket.addEventListener('close', () =>
+  console.log('Disconnected from Server 😭')
+);
+
+setTimeout(() => {
+  socket.send('hello from the browser!');
+}, 1000);
